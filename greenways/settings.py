fff299dcb4e9995470d95f3d912caa4178862dc4
Dickson Olejeme <olejemedickson@gmail.com>
2023-09-08 22:24:27 +0000

    Add JavaScript succcess message

--- greenways/settings.py
+++ greenways/settings.py
@@ -31,8 +31,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-ocassidydev-greenways-a1rwyegoqws.ws-eu104.gitpod.io',
-    '8000-ocassidydev-greenways-y13chb6125k.ws-eu104.gitpod.io', 
-    '127.0.0.1'
+    '8000-ocassidydev-greenways-y13chb6125k.ws-eu104.gitpod.io',
+    'https://ocassidydev-greenways-98htto6eqpj.ws-eu104.gitpod.io/'
+    '127.0.0.1','localhost'
     ]
 
 
@@ -83,16 +84,16 @@ WSGI_APPLICATION = 'greenways.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#   }
-# }
-
 DATABASES = {
-   'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+  }
 }
+
+# DATABASES = {
+#    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
 
 
