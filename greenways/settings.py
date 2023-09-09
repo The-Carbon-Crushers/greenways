d1b512b5822d0a64e9a29a330bf1a6bdb4bae3a0
Chrisfaherty <chrismactom@gmail.com>
2023-09-09 07:55:16 +0000

    Test deployment

--- greenways/settings.py
+++ greenways/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-if os.path.isfile('env.py'):
-    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,9 +88,6 @@ DATABASES = {
   }
 }
 
-# DATABASES = {
-#    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
 
 
 
