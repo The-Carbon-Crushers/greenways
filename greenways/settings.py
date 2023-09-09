9a64bcddcf9f1414ce28256544eb93de6b9d08a6
Chrisfaherty <chrismactom@gmail.com>
2023-09-09 10:09:07 +0000

    Add white noise to settings.py

--- greenways/settings.py
+++ greenways/settings.py
@@ -42,13 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic'
     'django.contrib.staticfiles',
     'greenways_app'
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
